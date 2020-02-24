f0ec671ac474dba6f69c590966f27a9d107c8bda
Jesse Schroder <jesse.schroder@shopify.com>
2020-02-24 16:37:57 -0500

    add some testing and then add polaris and then fix testing because polaris

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,4 +6,13 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'index returns formatted' do
+    get store_index_url
+    
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main div.Polaris-Card', Product.count
+    assert_select 'h2', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
+
 end
