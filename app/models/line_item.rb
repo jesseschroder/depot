22c6198f3b08b7917979db65d75332da63c2d52f
Jesse Schroder <jesse.schroder@shopify.com>
2020-03-06 19:55:19 -0500

    Task 6.1

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
+  belongs_to :order, optional: true
   belongs_to :product
   belongs_to :cart
 
