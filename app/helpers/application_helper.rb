45dd2f593c7bbe36458673f94bf3653ad81a7a6c
Jesse Schroder <jesse.schroder@shopify.com>
2020-03-09 10:43:54 -0400

    task 6.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def render_if(condition, record)
     render record if condition
   end
+
+  def checkout_page
+    controller_name == 'orders' && action_name == 'new'
+  end
 end
